70d000a3c1135b3424b1d739db59d0681208bb11
Nkateko.Ngwenya <Nkateko.Ngwenya@fb502afa-9955-0410-b79e-e38f497d9c7b>
2008-10-22 17:39:42 +0000

    Pickup.java
    Added generatePickupLocation(Map map) 
    This method generates pickupLocation and checks that its within bounds

--- crimsonportal/src/crimsonportal/googlecode/com/ObjectModel/Pickup.java
+++ crimsonportal/src/crimsonportal/googlecode/com/ObjectModel/Pickup.java
@@ -34,20 +34,39 @@ public abstract class Pickup
         pickupLocation.setY( loc.getY() );
     }
     
+    /**
+     * generates the pickup location and assigns the X and Y values
+     * 
+     * @param locX
+     * @param locY
+     */
     public void setPickupLocation(double locX, double locY) 
     {
         pickupLocation.setX(locX);
         pickupLocation.setY(locY);
     }
-    
+    /**
+     * This method generates pickupLocation and checks that its within bounds
+     * @param Mam map
+     * @return void
+     */
     public void generatePickupLocation (Map map)
     {
-        //TODO : Fix coordinates . . . 
-        
         Random randomGenerator = new Random();
         
         double randomX = randomGenerator.nextInt(map.height - 10);
         double randomY = randomGenerator.nextInt(map.width - 10);
+        
+        // make sure that the pickup doesnt go beyond the screen
+        if( randomX < 10 ) 
+        {
+            randomX+=(double)10;
+        }
+        
+        if( randomY < 10 ) 
+        {
+            randomY+=(double)10;
+        }
         
         setPickupLocation(randomX, randomY);
     }
