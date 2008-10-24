01a0220ba2f37418bc53473742a773609a8299a5
Nkateko.Ngwenya <Nkateko.Ngwenya@fb502afa-9955-0410-b79e-e38f497d9c7b>
2008-10-24 15:43:13 +0000

    MouseController.java
    Added MouseController class that will be used to simulate mouseEvents

---
+++ crimsonportal/src/crimsonportal/googlecode/com/Controller/MouseController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crimsonportal.googlecode.com.Controller;
+
+/**
+ *
+ * @author nkateko
+ */
+public class MouseController {
+
+}
