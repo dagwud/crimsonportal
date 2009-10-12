e0f85c0f9dd48dd7f3d284026a5bb7ff0a395813
johndevenish <johndevenish@fb502afa-9955-0410-b79e-e38f497d9c7b>
2009-10-12 14:43:53 +0000

    Implemented first draft of attack-and-retreat movement style for Fletcher. Code is messy, should be re-architected

--- trunk/crimsonportal/src/crimsonportal/googlecode/com/ObjectModel/EnemyUnit.java
+++ trunk/crimsonportal/src/crimsonportal/googlecode/com/ObjectModel/EnemyUnit.java
@@ -7,6 +7,7 @@ package crimsonportal.googlecode.com.ObjectModel;
 
 import crimsonportal.googlecode.com.Debug;
 import crimsonportal.googlecode.com.Utils;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  *
@@ -21,6 +22,14 @@ public abstract class EnemyUnit extends Unit
         this.moveSpeed = moveSpeed;
         this.attackSpeed = attackSpeed;
         this.health = startingHealth;
+        
+        // If using attack-and-retreat movement style, check that the required
+        // methods have been overridden:
+        if (getMovementType() == MovementType.MOVEMENT_ATTACKANDRETREAT) {
+            if (getAttackAndRetreat_RetreatDistance() == null) {
+                throw new RuntimeException("Enemy has momentType = MOVEMENT_ATTACKANDRETREAT but has not overridden getAttackAndRetreat_RetreatDistance() method");
+            }
+        }
     }
     
     public int getAttackDamage()
@@ -45,6 +54,16 @@ public abstract class EnemyUnit extends Unit
     
     public void attack(PlayerUnit player)
     {
+        if (getMovementType() == MovementType.MOVEMENT_ATTACKANDRETREAT) {
+            if (attackAndRetreat_retreatProgress >= 0) {
+                // Retreating; don't allow an attack
+                return;
+            } else {
+                // Start retreat (after this attack):
+                attackAndRetreat_retreatProgress = getAttackAndRetreat_RetreatDistance(); 
+            }
+        }
+        
         long timeSinceLastAttackMS = System.currentTimeMillis() - lastAttackTime;
         double attackDurationMS = 1000.0 / attackSpeed;
         if (timeSinceLastAttackMS >= attackDurationMS)
@@ -65,13 +84,21 @@ public abstract class EnemyUnit extends Unit
             case MOVEMENT_STRAIGHTLINE:
                 moveStraightLine();
                 break;
-            
+                
+            case MOVEMENT_ATTACKANDRETREAT:
+                moveAttackAndRetreat();
+                break;
+                
             default:
                 throw new RuntimeException("Unknown movement type " + movetype);
         }
     }
     
-    protected void moveStraightLine()
+    private void moveStraightLine() {
+        moveStraightLine(false);
+    }
+    
+    private void moveStraightLine(boolean isRetreating)
     {
         if (Debug.checkFlag(Debug.flagKey.DISABLE_ENEMY_MOVEMENT)) return;
         Location targetLoc = getStrategy().getTarget().getCentreOfObject();
@@ -79,10 +106,14 @@ public abstract class EnemyUnit extends Unit
         double diffY = targetLoc.getY() - thisLoc.getY();
         double diffX = targetLoc.getX() - thisLoc.getX();
         double moveAmount = moveSpeed;
+        // If not retreating, check that moving forward would actually mean getting
+        // closer to the target:
         double distBeforeMove = Math.sqrt((diffY * diffY) + (diffX * diffX));
-        if (getRadius() + getStrategy().getTarget().getRadius() >= distBeforeMove )
-        {
-            return;
+        if (!isRetreating) {
+            if (getRadius() + getStrategy().getTarget().getRadius() >= distBeforeMove )
+            {
+                return;
+            }
         }
         
         double moveAngleRadians = 0;
@@ -106,23 +137,47 @@ public abstract class EnemyUnit extends Unit
                 return; 
             }
         }
+        if (isRetreating) {
+            // Retreat: Adjust moveAngleRadians by Math.PI (180deg) which will 
+            // take the EnemyUnit away from its target:
+            moveAngleRadians = moveAngleRadians + Math.PI; 
+        }
         double moveX = (double) Math.round(moveAmount * Utils.cos(moveAngleRadians));
         double moveY = (double) Math.round(moveAmount * Utils.sin(moveAngleRadians));
         
         // Calculate how far from the target the enemyUnit will be after the move: 
-        double newDiffY = getStrategy().getTarget().getCentreOfObject().getY() - (this.getCentreOfObject().getY() + moveY);
-        double newDiffX = getStrategy().getTarget().getCentreOfObject().getX() - (this.getCentreOfObject().getX() + moveX);
-        double distAfterMove = Math.sqrt((newDiffY * newDiffY) + (newDiffX * newDiffX));
-        if ( distAfterMove >= distBeforeMove )
-        {
-            moveX = 0.0;
-            moveY = 0.0;
+        double newDiffY = targetLoc.getY() - (this.getCentreOfObject().getY() + moveY);
+        double newDiffX = targetLoc.getX() - (this.getCentreOfObject().getX() + moveX);
+        if (!isRetreating) {
+            double distAfterMove = Math.sqrt((newDiffY * newDiffY) + (newDiffX * newDiffX));
+            // If the EnemyUnit is trying to get to its target, ensure that it 
+            // doesn't accidentally move further away:
+            if ( distAfterMove >= distBeforeMove )
+            {
+                moveX = 0.0;
+                moveY = 0.0;
+            }
         }
         
         Location newLocation = new Location(
             getCentreOfObject().getX() + moveX,
             getCentreOfObject().getY() + moveY);
         moveTo(newLocation);
+    }
+    
+    private void moveAttackAndRetreat() {
+        boolean isRetreating = false;
+        if (attackAndRetreat_retreatProgress >= 0) {
+            // Retreating:
+            isRetreating = true;
+            attackAndRetreat_retreatProgress--;
+            if (attackAndRetreat_retreatProgress <= 0) {
+                // Stop retreating:
+                attackAndRetreat_retreatProgress = -1; // Start attack
+                isRetreating = false;
+            }
+        }
+        moveStraightLine(isRetreating);
     }
     
     @Override 
@@ -134,11 +189,17 @@ public abstract class EnemyUnit extends Unit
         return rotation;
     }
     
+    protected Integer getAttackAndRetreat_RetreatDistance() {
+        return null;
+    }
+    
     public abstract String getSpriteFilename();
             
     protected int attackDamage;
     protected double attackSpeed;
     private long lastAttackTime;
+    
+    private int attackAndRetreat_retreatProgress = 0;
     
     protected static final int PRESET_MOVESPEED_AMBLE = 1;
     protected static final int PRESET_MOVESPEED_WALK = 2;
@@ -156,5 +217,5 @@ public abstract class EnemyUnit extends Unit
     protected static final double PRESET_SIZE_MEDIUM = 15d;
     protected static final double PRESET_SIZE_LARGE = 20d;
     protected static final double PRESET_SIZE_HUGE = 35d;
-    public static enum MovementType {MOVEMENT_STRAIGHTLINE};
+    public static enum MovementType {MOVEMENT_STRAIGHTLINE, MOVEMENT_ATTACKANDRETREAT};
 }
